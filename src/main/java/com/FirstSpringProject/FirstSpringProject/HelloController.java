1a17bf4a37ed1f3c2728b8b1bd64b970b46b16df
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-04 16:35:20 +0530

    Create /hello post mapping controller

--- src/main/java/com/FirstSpringProject/FirstSpringProject/HelloController.java
+++ src/main/java/com/FirstSpringProject/FirstSpringProject/HelloController.java
@@ -1,7 +1,6 @@
 package com.FirstSpringProject.FirstSpringProject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Restcontroller = Controller + ResponseBody
 @RestController
@@ -9,5 +8,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello abhishek";
+    }
+
+    @PostMapping("/hello")
+    public String sayHello(@RequestBody String name) {
+        return "Hello " + name;
     }
 }
